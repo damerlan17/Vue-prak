f39b7e3938c57f625cac306ceef06fee7f51895e
damerlan17 <uchunakdamelan@gmail/com>
2026-02-16 20:10:06 +0700

    practical work 5

--- js/main.js
+++ js/main.js
@@ -2,26 +2,37 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        aText:"More products like this",
         inStock: true,
-        inventory: 100,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'gender-neutral'],
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
-                variantId: '2234',
+                variantId: 2234,
                 variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
-                variantId: '2235',
+                variantId: 2235,
                 variantColor: 'blue',
-
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
-    }
+        cart: 0,
+        methods: {
+            addToCart() {
+                this.cart += 1
+            }
+        },
+
+        deleteToCart() {
+            this.cart -= 1
+        },
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    },
+
 })
