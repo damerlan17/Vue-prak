28c3fcb8ee3294047a21759d62e6c1fe8252d5dd
gr441_uaeer <uchunakdamerlan@gmail.com>
2026-02-16 15:58:25 +0700

    Practical work №3

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         aText:"More products like this",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
 
     }
 })
