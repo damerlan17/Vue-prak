72952d55b92515e185bd2116608873a4fd397798
gr441_uaeer <uchunakdamerlan@gmail.com>
2026-02-16 15:25:41 +0700

    Practical work №2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-let product = "Socks";
-let description = "A pair of werm, fuzzy"
-
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: description,
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        aText:"More products like this",
+
     }
 })
